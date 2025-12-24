35a3cf53f5dfeb9e816f8961ae3acca4d0456191
Tata Adi Nugroho <tataa2237@gmail.com>
2025-12-24 22:19:48 +0700

    improve code implementation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,37 +10,31 @@ use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
-	/**
-	 * Register any application services.
-	 */
-	public function register(): void
-	{
-		//
-	}
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
 
-	/**
-	 * Bootstrap any application services.
-	 */
-	public function boot(): void
-	{
-		Inertia::share(
-			[
-				'auth.user' => function () {
-					return Auth::user() ? Auth::user()->load('roles') : null;
-				},
-                'auth.roles' => function () {
-                    return Auth::user() ? Auth::user()->roles : [];
-                },
-				'flash' => function () {
-					return [
-						'success' => session('success'),
-						'error' => session('error'),
-					];
-				},
-				'errors' => function () {
-					return Request::session()->get('errors') ? Request::session()->get('errors')->getBag('default')->getMessages() : (object)[];
-				}
-			]
-		);
-	}
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Inertia::share([
+            "auth.user" => fn() => Auth::user(),
+            "flash" => fn() => [
+                "success" => session("success"),
+                "error" => session("error"),
+            ],
+            "errors" => fn() => Request::session()->get("errors")
+                ? Request::session()
+                    ->get("errors")
+                    ->getBag("default")
+                    ->getMessages()
+                : (object) [],
+        ]);
+    }
 }
